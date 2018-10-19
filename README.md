070725c9a39a9695e67ea11879d27c71b8ea9bc0
缪茂威 <morrisware01@gmail.com>
2018-10-19 12:00:24 +0800

    save

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 ## Glide
 
-### LruCache实现原理
-
-### DiskLruCache实现原理
-
-### Glide缓存机制
-
 ### Glide加载图片流程
 
 1. 先通过Glide.with(context)获取RequestManager
@@ -17,12 +11,31 @@
 1. 如果没有获取到，先查询Jobs中是否已经存在EngineKey的engineJob，有的话添加ResourceCallback等待任务完成，没有的话
 构造engineJob和decodeJob开始资源model任务
 1. decodeJob中通过ResourceCacheGenerator、DataCacheGenerator、SourceGenerator加载model
-1. DataFetcherGenerator会从Glide构造时注册的Registry中查找加载model的方式
+1. 各种DataFetcherGenerator会从Glide构造时注册的Registry中查找modelLoader
 1.
+
+### Glide缓存机制
+
+1. 从target的getRequest获取上一次的Request
+1. Engine.load()中先构造EngineKey
+1. 从Engine的activeResources中根据EngineKey获取EngineResource（activeResources中维持一个HashMap）
+1. 从Glide.memoryCache中根据EngineKey获取EngineResource（memoryCache实现了LruCache）
+1. 内存缓存获取失败后，构造engineJob和decodeJob开始从磁盘缓存和网络请求获取
+1. decodeJob的run方法会从ResourceCacheGenerator、DataCacheGenerator、SourceGenerator中获取Resource
+1. ResourceCacheGenerator中构造ResourceCacheKey从本地DiskLruCache中获取（
+Glide默认位置为context.getCacheFile（"image_manager_disk_cache"）,最大大小为250MB）
+1.
+
+
+### Glide如何确定target的宽高
+
+### Glide线程池
 
 ### GlideModule
 
-### Glide线程池
+### LruCache实现原理
+
+### DiskLruCache实现原理
 
 
 
--- app/src/main/java/com/morrisware/android/glidelearn/MainActivity.kt
+++ app/src/main/java/com/morrisware/android/glidelearn/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.morrisware.android.glidelearn
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,26 +14,54 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Glide.with(this)
-            .load(R.mipmap.ic_launcher_round)
-            .apply(RequestOptions()
-                .diskCacheStrategy(DiskCacheStrategy.NONE))
-            .transition(DrawableTransitionOptions())
-            .thumbnail(Glide.with(this).load(R.mipmap.ic_launcher))
-            .error(Glide.with(this).load(R.mipmap.ic_launcher))
-            .listener(object : RequestListener<Drawable?> {
-                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable?>?, isFirstResource: Boolean): Boolean {
-                    return true
-                }
-
-                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable?>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                    return true
-                }
-            })
-            .into(imageView)
-        Array<Any?>(10) {
-            null
-        }
-        arrayOfNulls<Any>(10)
+        load.setOnClickListener { load() }
+        debugResourceCache.setOnClickListener { debugResourceCacheGenerator() }
+        debugDataCache.setOnClickListener { debugDataCacheGenerator() }
+        debugSource.setOnClickListener { debugSourceGenerator() }
     }
+
+    private fun load() {
+        Glide.with(this)
+            .load(R.mipmap.normal)
+            .apply(RequestOptions()
+                .transform(CircleCrop()))
+            .transition(DrawableTransitionOptions())
+            .thumbnail(Glide.with(this).load(R.mipmap.thumb))
+            .error(Glide.with(this).load(R.mipmap.error))
+            .into(imageView)
+    }
+
+    private fun debugResourceCacheGenerator() {
+        Glide.with(this)
+            .load(R.mipmap.normal)
+            .apply(RequestOptions()
+                .transform(CircleCrop())
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE))
+            .transition(DrawableTransitionOptions().crossFade())
+            .into(imageView)
+    }
+
+    private fun debugDataCacheGenerator() {
+        Glide.with(this)
+            .load(R.mipmap.normal)
+            .apply(RequestOptions()
+                .transform(CircleCrop())
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.DATA))
+            .transition(DrawableTransitionOptions().crossFade())
+            .into(imageView)
+    }
+
+    private fun debugSourceGenerator() {
+        Glide.with(this)
+            .load(R.mipmap.normal)
+            .apply(RequestOptions()
+                .transform(CircleCrop())
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE))
+            .transition(DrawableTransitionOptions().crossFade())
+            .into(imageView)
+    }
+
 }
